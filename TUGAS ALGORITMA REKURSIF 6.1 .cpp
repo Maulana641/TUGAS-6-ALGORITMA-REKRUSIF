e991bc55676b5763d08ed4f57b72c48630e52efd
Maulana641 <62836628+Maulana641@users.noreply.github.com>
2020-05-05 20:36:50 +0800

    Add files via upload

---
+++ TUGAS ALGORITMA REKURSIF 6.1 .cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+long int faktorial( long int a);
+
+main(){
+ long int faktor;
+ long int n;
+   cout<<"Masukan Nilai Faktorial = ";
+   cin>>n;
+   faktor = faktorial(n);
+   cout<<"Nilai Faktorial Dari "<< n <<"! = "<<faktor<<endl;
+}
+
+long int faktorial( long int a){
+ {   if ((a == 0) || (a == 1 )) 
+	  return (1); 
+	  else
+	  return (a * faktorial(a-1));  }
+}
